077d7c43a91f01b77f24da99cd7edf85978411a8
Cagatay Ciftci <im.cgtycftc@gmail.com>
2019-01-11 01:30:25 +0300

    Remove dashboard

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import Home from './home'
 import Issues from './issues'
 import IssueDetails from './issue-details'
 import KnowledgeBase from './knowledge-base'
-import Dashboard from './dashboard'
 import About from './about'
 
-export { Login, Home, Issues, IssueDetails, KnowledgeBase, Dashboard, About }
+export { Login, Home, Issues, IssueDetails, KnowledgeBase, About }
